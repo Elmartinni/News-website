685cf913700f0736504d01a034ad22ff9298ec40
Elmartinni <quicksaver03@gmail.com>
2024-08-08 08:30:01 +0100

    join

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 // News API Configuration
 const NEWS_API_URL = 'https://newsapi.org/v2/top-headlines';
-const NEWS_API_KEY = 'YOUR_NEWS_API_KEY';
+const NEWS_API_KEY = '052a13a87505438a8cc1e768c1f06c24';
 
 // API Route to Fetch News
 app.get('/api/news', async (req, res) => {
@@ -32,7 +32,7 @@ app.get('/api/news', async (req, res) => {
 
 // Serve Front-End
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
 });
 
 app.listen(PORT, () => {
